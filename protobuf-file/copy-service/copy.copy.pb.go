6560f2b2f43e55611453be2f8310657263c89ac5
ductv4 <ductv4@vng.com.vn>
2019-09-26 16:31:04 +0700

    con loi cho fiel phia sau not checknil

--- protobuf-file/copy-service/copy.copy.pb.go
+++ protobuf-file/copy-service/copy.copy.pb.go
@@ -4,14 +4,15 @@
 package copy
 
 import (
+	context "context"
 	fmt "fmt"
 	math "math"
+	reflect "reflect"
+
 	proto "github.com/gogo/protobuf/proto"
+	"github.com/tvducmt/protoc-gen-copy/protobuf-file/core-service"
 	core_service "github.com/tvducmt/protoc-gen-copy/protobuf-file/core-service"
 	middleware "github.com/tvducmt/protoc-gen-copy/protobuf-file/middleware"
-	reflect "reflect"
-	context "context"
-	git_zapa_cloud_merchant_tools_helper_proto "git.zapa.cloud/merchant-tools/helper/proto"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -38,58 +39,8 @@ func NewCopy() *copy {
 }
 
 func (c *copy) ListCITransactionsRequest(from *middleware.BODetailReconciliation, to *core_service.BODetailReconciliation) error {
-	if !isNil(from.FromDate) {
-		to.FromDate = &git_zapa_cloud_merchant_tools_helper_proto.Date{
-			Year: func(h *git_zapa_cloud_merchant_tools_helper_proto.Date) int32 {
-				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.Int32)).Interface().(int32)
-				}
-				return h.Year
-			}(from.FromDate),
-			Month: func(h *git_zapa_cloud_merchant_tools_helper_proto.Date) int32 {
-				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.Int32)).Interface().(int32)
-				}
-				return h.Month
-			}(from.FromDate),
-			Day: func(h *git_zapa_cloud_merchant_tools_helper_proto.Date) int32 {
-				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.Int32)).Interface().(int32)
-				}
-				return h.Day
-			}(from.FromDate),
-		}
-	}
-	if !isNil(from.TransTime) {
-		to.TransTime = &timestamp.Timestamp{
-			Seconds: func(h *timestamp.Timestamp) int64 {
-				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.Int64)).Interface().(int64)
-				}
-				return h.Seconds
-			}(from.TransTime),
-			Nanos: func(h *timestamp.Timestamp) int32 {
-				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.Int32)).Interface().(int32)
-				}
-				return h.Nanos
-			}(from.TransTime),
-		}
-	}
 	if !isNil(from.CountableAttribute) {
 		to.CountableAttribute = &core.BODetailReconciliation_CountableAttribute{
-			MerchantRefundAmount: func(h *middleware.BODetailReconciliation_CountableAttribute) int64 {
-				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.Int64)).Interface().(int64)
-				}
-				return h.MerchantRefundAmount
-			}(from.CountableAttribute),
-			ReturnPercentFeeNotVat: func(h *middleware.BODetailReconciliation_CountableAttribute) double {
-				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.Double)).Interface().(double)
-				}
-				return h.ReturnPercentFeeNotVat
-			}(from.CountableAttribute),
 			TpeBankCode: func(h *middleware.BODetailReconciliation_CountableAttribute) string {
 				if h == nil {
 					return reflect.Zero(reflect.TypeOf(reflect.String)).Interface().(string)
@@ -110,26 +61,12 @@ func (c *copy) ListCITransactionsRequest(from *middleware.BODetailReconciliation
 					return h.H2
 				}(from.CountableAttribute.Hello),
 			},
-		}
-	}
-	if !isNil(from.Ductran) {
-		to.Ductran = &core.BODetailReconciliation_Profile{
-			Name: func(h *middleware.BODetailReconciliation_Profile) string {
+			ItemCount: func(h *middleware.BODetailReconciliation_CountableAttribute) int32 {
 				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.String)).Interface().(string)
+					return reflect.Zero(reflect.TypeOf(reflect.Int32)).Interface().(int32)
 				}
-				return h.Name
-			}(from.Ductran),
-		}
-	}
-	if !isNil(from.Ductran1) {
-		to.Ductran1 = &core.BODetailReconciliation_Profile{
-			Name: func(h *middleware.BODetailReconciliation_Profile) string {
-				if h == nil {
-					return reflect.Zero(reflect.TypeOf(reflect.String)).Interface().(string)
-				}
-				return h.Name
-			}(from.Ductran1),
+				return h.ItemCount
+			}(from.CountableAttribute),
 		}
 	}
 	return nil
